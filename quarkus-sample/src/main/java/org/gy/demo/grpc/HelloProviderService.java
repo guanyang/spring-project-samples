c1d5127a61f964b82d2b2d5190055605799c8c4b
guanyang <guanyangsunlight@163.com>
2023-08-11 16:43:14 +0800

    update quarkus-sample

--- quarkus-sample/src/main/java/org/gy/demo/grpc/HelloProviderService.java
+++ quarkus-sample/src/main/java/org/gy/demo/grpc/HelloProviderService.java
@@ -3,6 +3,7 @@ package org.gy.demo.grpc;
 
 import io.quarkus.grpc.GrpcService;
 import io.smallrye.mutiny.Uni;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 功能描述：
@@ -11,11 +12,13 @@ import io.smallrye.mutiny.Uni;
  * @version 1.0.0
  * @date 2023/8/8 09:53
  */
+@Slf4j
 @GrpcService
 public class HelloProviderService implements Greeter {
 
     @Override
     public Uni<HelloReply> sayHello(HelloRequest request) {
+        log.info("[Greeter]sayHello: {}", request.getName());
         return Uni.createFrom().item(() -> HelloReply.newBuilder().setMessage("Hello " + request.getName()).build());
     }
 }
