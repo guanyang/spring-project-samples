49e3c4abb9b26c8e99184fdf92821640bd83bf18
guanyang <guanyangsunlight@163.com>
2023-09-18 16:35:41 +0800

    add Spring Data R2DBC demo

--- native-sample/src/main/java/org/gy/demo/nativesample/service/impl/HelloWorldNewServiceImpl.java
+++ native-sample/src/main/java/org/gy/demo/nativesample/service/impl/HelloWorldNewServiceImpl.java
@@ -9,8 +9,10 @@ import org.gy.demo.nativesample.service.HelloWorldNewService;
 import org.gy.demo.nativesample.util.PageQuery;
 import org.gy.demo.nativesample.util.PageResult;
 import org.gy.demo.nativesample.util.Result;
-import org.springframework.data.domain.Example;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.relational.core.query.Query;
+import org.springframework.data.relational.core.query.Update;
+import org.springframework.data.relational.core.sql.SqlIdentifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +20,14 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import static org.springframework.data.domain.Sort.Order.desc;
+import static org.springframework.data.domain.Sort.by;
+import static org.springframework.data.relational.core.query.Criteria.where;
 
 @Slf4j
 @Service
@@ -31,40 +41,68 @@ public class HelloWorldNewServiceImpl implements HelloWorldNewService {
 
     @Override
     public Mono<PageResult<HelloWorldNew>> list(PageQuery query) {
-        HelloWorldNew example = new HelloWorldNew();
-        example.setDeleted(DeletedEnum.NO.getCode());
-        Mono<Long> count = repository.count(Example.of(example));
-        return count.flatMap(c -> {
+        Query queryWrapper = Query.query(where(HelloWorldNew.DELETED).is(DeletedEnum.NO.getCode()));
+        return template.count(queryWrapper, HelloWorldNew.class).flatMap(c -> {
             if (c == 0) {
                 return Mono.just(PageResult.success(0, Collections.emptyList()));
             } else {
-                Flux<HelloWorldNew> page = repository.page(query);
+                Query pageQuery = queryWrapper.sort(by(desc(HelloWorldNew.ID))).offset(query.getOffset()).limit(query.getPageSize());
+                Flux<HelloWorldNew> page = template.select(pageQuery, HelloWorldNew.class);
                 return page.collectList().map(list -> PageResult.success(c, list));
             }
         });
+
+//        HelloWorldNew example = new HelloWorldNew();
+//        example.setDeleted(DeletedEnum.NO.getCode());
+//        Mono<Long> count = repository.count(Example.of(example));
+//        return count.flatMap(c -> {
+//            if (c == 0) {
+//                return Mono.just(PageResult.success(0, Collections.emptyList()));
+//            } else {
+//                Flux<HelloWorldNew> page = repository.page(query);
+//                return page.collectList().map(list -> PageResult.success(c, list));
+//            }
+//        });
     }
 
     @Override
     public Mono<Result<HelloWorldNew>> get(Long id) {
-        return repository.findById(id).map(Result::success).defaultIfEmpty(Result.error(HttpStatus.BAD_REQUEST));
+        Query queryWrapper = Query.query(where(HelloWorldNew.ID).is(id));
+        return template.selectOne(queryWrapper, HelloWorldNew.class).map(Result::success).defaultIfEmpty(Result.error(HttpStatus.BAD_REQUEST));
+//        return repository.findById(id).map(Result::success).defaultIfEmpty(Result.error(HttpStatus.BAD_REQUEST));
     }
 
     @Override
     @Transactional(rollbackFor = Exception.class)
     public Mono<Result<HelloWorldNew>> add(HelloWorldNew entity) {
-        return repository.save(entity).map(Result::success);
+        return template.insert(entity).map(Result::success);
+//        return repository.save(entity).map(Result::success);
     }
 
     @Override
     @Transactional(rollbackFor = Exception.class)
     public Mono<Result<Boolean>> update(HelloWorldNew entity) {
-        return repository.update(entity).map(r -> r > 0).map(Result::success);
+        Query queryWrapper = Query.query(where(HelloWorldNew.ID).is(entity.getId()));
+        Map<SqlIdentifier, Object> updateMap = new HashMap<>();
+        updateWrapper(updateMap, HelloWorldNew.NAME, entity::getName);
+        updateWrapper(updateMap, HelloWorldNew.DELETED, entity::getDeleted);
+        updateWrapper(updateMap, HelloWorldNew.VERSION, entity::getVersion);
+        updateWrapper(updateMap, HelloWorldNew.UPDATE_BY, entity::getUpdateBy);
+        return template.update(queryWrapper, Update.from(updateMap), HelloWorldNew.class).map(r -> r > 0).map(Result::success);
+
+//        return repository.update(entity).map(r -> r > 0).map(Result::success);
     }
 
     @Override
     @Transactional(rollbackFor = Exception.class)
     public Mono<Result<Boolean>> delete(Long id) {
-        return repository.deleteById(id).thenReturn(Result.success(Boolean.TRUE));
+        Query queryWrapper = Query.query(where(HelloWorldNew.ID).is(id));
+        return template.delete(queryWrapper, HelloWorldNew.class).map(r -> r > 0).map(Result::success);
+//        return repository.deleteById(id).thenReturn(Result.success(Boolean.TRUE));
+    }
+
+    private static void updateWrapper(Map<SqlIdentifier, Object> updateMap, String column, Supplier<Object> value) {
+        Optional.ofNullable(value.get()).ifPresent(v -> updateMap.put(SqlIdentifier.unquoted(column), v));
     }
 
 }
