e8b68dbc45eb569d4ae03eea3d8ef58edbd33d58
guanyang <guanyangsunlight@163.com>
2025-12-15 16:40:30 +0800

    update dubbo demo

--- dubbo-sample/dubbo-sample-api/src/main/java/org/gy/demo/dubbo/api/service/SampleService.java
+++ dubbo-sample/dubbo-sample-api/src/main/java/org/gy/demo/dubbo/api/service/SampleService.java
@@ -3,6 +3,8 @@ package org.gy.demo.dubbo.api.service;
 import org.gy.demo.dubbo.api.dto.TestRequest;
 import org.gy.demo.dubbo.api.dto.TestResponse;
 import org.gy.demo.dubbo.api.dto.support.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 功能描述：
@@ -10,8 +12,10 @@ import org.gy.demo.dubbo.api.dto.support.Result;
  * @author gy
  * @version 1.0.0
  */
+@RequestMapping("/api/test")
 public interface SampleService {
 
+    @GetMapping("/hello")
     Result<TestResponse> hello(TestRequest request);
 
 }
